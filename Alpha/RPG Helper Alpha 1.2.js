191a616dcff8a1896dc62781c7b482af778e9f48
Genbu Hase <hasegenbu@gmail.com>
2016-08-04 12:39:38 +0900

    Update RPG Helper Alpha 1.2.js

--- Alpha/RPG Helper Alpha 1.2.js
+++ Alpha/RPG Helper Alpha 1.2.js
@@ -297,11 +297,13 @@ var RPGHelper = function () {
 				
 				this.Canvas.appendChild(MapCanvas);
 				
-			var Ctx = MapCanvas.getContext("2d");
-			
-			for (var y = 0; y < MapData[0].length; y++) {
-				for (var x = 0; x < MapData[0][y].length; x++) {
-					Ctx.drawImage(TipImg, 16 * (MapData[0][y][x] % 8), 16 * (Math.floor(MapData[0][y][x] / 8)), 16, 16, 16 * x, 16 * y, 16, 16);
+			TipImg.onload = function () {
+				var Ctx = MapCanvas.getContext("2d");
+				
+				for (var y = 0; y < MapData[0].length; y++) {
+					for (var x = 0; x < MapData[0][y].length; x++) {
+						Ctx.drawImage(TipImg, 16 * (MapData[0][y][x] % 8), 16 * (Math.floor(MapData[0][y][x] / 8)), 16, 16, 16 * x, 16 * y, 16, 16);
+					}
 				}
 			}
 		}
