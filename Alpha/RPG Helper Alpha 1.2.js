438d31eff01843ceb0ac0b9ed8a8a59943076717
Genbu Hase <hasegenbu@gmail.com>
2016-08-06 12:25:50 +0900

    Update RPG Helper Alpha 1.2.js

--- Alpha/RPG Helper Alpha 1.2.js
+++ Alpha/RPG Helper Alpha 1.2.js
@@ -308,7 +308,7 @@ var RPGHelper = function () {
 				for (var y = 0; y < MapData[0].length; y++) {
  					for (var x = 0; x < MapData[0][y].length; x++) {
  						if (typeof MapData[0][y][x] != "^" && typeof MapData[0][y][x] == "number") {
- 							Ctx.drawImage(TipImg, 16 * (MapData[0][y][x] % 8), 16 * (Math.floor(MapData[0][y][x] / 8)), 16, 16, 16 * x, 8 * y, 16, 8);
+ 							Ctx.drawImage(TipImg, 16 * (MapData[0][y][x] % 8), 16 * (Math.floor(MapData[0][y][x] / 8)), 16, 16, 16 * x, 16 * y, 16, 16);
  						}
  					}
  				}
@@ -316,7 +316,7 @@ var RPGHelper = function () {
 				for (var y = 0; y < MapData[1].length; y++) {
 					for (var x = 0; x < MapData[1][y].length; x++) {
 						if (typeof MapData[1][y][x] != "^" && typeof MapData[1][y][x] == "number") {
-							Ctx.drawImage(TipImg, 16 * (MapData[1][y][x] % 8), 16 * (Math.floor(MapData[1][y][x] / 8)), 16, 16, 16 * x, 8 * y, 16, 8);
+							Ctx.drawImage(TipImg, 16 * (MapData[1][y][x] % 8), 16 * (Math.floor(MapData[1][y][x] / 8)), 16, 16, 16 * x, 16 * y, 16, 16);
 						}
 					}
 				}
@@ -324,7 +324,7 @@ var RPGHelper = function () {
 				for (var y = 0; y < MapData[2].length; y++) {
 					for (var x = 0; x < MapData[2][y].length; x++) {
 						if (typeof MapData[2][y][x] != "^" && typeof MapData[2][y][x] == "number") {
-							Ctx.drawImage(TipImg, 16 * (MapData[2][y][x] % 8), 16 * (Math.floor(MapData[2][y][x] / 8)), 16, 16, 16 * x, 8 * y, 16, 8);
+							Ctx.drawImage(TipImg, 16 * (MapData[2][y][x] % 8), 16 * (Math.floor(MapData[2][y][x] / 8)), 16, 16, 16 * x, 16 * y, 16, 16);
 						}
 					}
 				}
