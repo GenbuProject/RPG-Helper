2872ccde7bf2936e6204775ba28f2aeae0a6e344
Genbu Hase <hasegenbu@gmail.com>
2016-08-09 18:08:34 +0900

    Update RPG Helper Alpha 1.2.js

--- Alpha/RPG Helper Alpha 1.2.js
+++ Alpha/RPG Helper Alpha 1.2.js
@@ -95,11 +95,11 @@ var RPGHelper = function () {
 		
 		PlayBGM: function (ID) {
 			if (!this.BGM.paused) {
-				this.BGM.muted = true;
+				//this.BGM.muted = true;
 				this.BGM.currentTime = 0;
 				this.BGM.pause();
 				this.BGM.src = "Audio/null.wav";
-				this.BGM.muted = false;
+				//this.BGM.muted = false;
 			}
 			
 			for (var Key in Resource.SystemData.Audio.BGM) {
