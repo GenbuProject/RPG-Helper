a354d30e1e5b8715a73ffad7a24063b44dbed915
Genbu Hase <hasegenbu@gmail.com>
2016-08-13 15:59:35 +0900

    Update Worker Pack for RPG Helper Alpha 1.3.js

--- Alpha/Worker Pack for RPG Helper Alpha 1.3.js
+++ Alpha/Worker Pack for RPG Helper Alpha 1.3.js
@@ -19,7 +19,7 @@ onmessage = function (Event) {
 		case WorkID.ServiceForLoad:
 			postMessage({
 				WorkID: WorkID.ServiceForLoad,
-				Data: "Searched."
+				Data: "User is choosing a file..."
 			});
 			
 			break;
