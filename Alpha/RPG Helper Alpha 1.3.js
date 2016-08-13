0ad9b38f52d0bc49beb713d2f1096058816dc553
Genbu Hase <hasegenbu@gmail.com>
2016-08-13 13:08:21 +0900

    Update RPG Helper Alpha 1.3.js

--- Alpha/RPG Helper Alpha 1.3.js
+++ Alpha/RPG Helper Alpha 1.3.js
@@ -570,7 +570,7 @@ var RPGHelper = function () {
 						Event.preventDefault();
 						
 						CharaPos[1]--;
-						Character.Warp(0, this.R.DIRECTION.N, [CharaPos[0], CharaPos[1]]);
+						Character.Warp(0, R.DIRECTION.N, [CharaPos[0], CharaPos[1]]);
 						
 						break;
 						
@@ -578,7 +578,7 @@ var RPGHelper = function () {
 						Event.preventDefault();
 						
 						CharaPos[1]++;
-						Character.Warp(0, this.R.DIRECTION.S, [CharaPos[0], CharaPos[1]]);
+						Character.Warp(0, R.DIRECTION.S, [CharaPos[0], CharaPos[1]]);
 						
 						break;
 						
@@ -586,7 +586,7 @@ var RPGHelper = function () {
 						Event.preventDefault();
 						
 						CharaPos[0]--;
-						Character.Warp(0, this.R.DIRECTION.W, [CharaPos[0], CharaPos[1]]);
+						Character.Warp(0, R.DIRECTION.W, [CharaPos[0], CharaPos[1]]);
 						
 						break;
 						
@@ -594,7 +594,7 @@ var RPGHelper = function () {
 						Event.preventDefault();
 						
 						CharaPos[0]++;
-						Character.Warp(0, this.R.DIRECTION.E, [CharaPos[0], CharaPos[1]]);
+						Character.Warp(0, R.DIRECTION.E, [CharaPos[0], CharaPos[1]]);
 						
 						break;
 				}
