ca3dcb10567a5e6cb4d97540659b59f55c0ebc09
Genbu Hase <hasegenbu@gmail.com>
2016-08-16 11:19:31 +0900

    Update RPG Helper Alpha 1.3.js

--- Alpha/RPG Helper Alpha 1.3.js
+++ Alpha/RPG Helper Alpha 1.3.js
@@ -781,7 +781,7 @@ var RPGHelper = function () {
 						}
 						
 						if (TipSettingData[MapData[2][CharaPos[1] - 1][CharaPos[0]]] == "100000") {
-							Character.Warp(CharacterID, R.DIRECTION.N, [CharaPos[0], CharaPos[1]], [0x0001, TipSettingData[MapData[2][CharaPos[1]][CharaPos[0]]]] == "100000" ? 0x0001 : TipSettingData[MapData[2][CharaPos[1]][CharaPos[0]]]] == "100" ? 0x0001 : TipSettingData[MapData[2][CharaPos[1]][CharaPos[0]]]] == "400000" ? 0x0002 : 0x0000]);
+							Character.Warp(CharacterID, R.DIRECTION.N, [CharaPos[0], CharaPos[1]], [0x0001, TipSettingData[MapData[2][CharaPos[1]][CharaPos[0]]] == "100000" ? 0x0001 : TipSettingData[MapData[2][CharaPos[1]][CharaPos[0]]]] == "100" ? 0x0001 : TipSettingData[MapData[2][CharaPos[1]][CharaPos[0]]]] == "400000" ? 0x0002 : 0x0000]);
 						}
 						
 						break;
