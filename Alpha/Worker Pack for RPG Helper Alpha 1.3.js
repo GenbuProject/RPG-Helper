867f1a36c90b38cc64aa0f192e18778479858810
Genbu Hase <hasegenbu@gmail.com>
2016-08-17 12:01:18 +0900

    Update Worker Pack for RPG Helper Alpha 1.3.js

--- Alpha/Worker Pack for RPG Helper Alpha 1.3.js
+++ Alpha/Worker Pack for RPG Helper Alpha 1.3.js
@@ -1,18 +1,8 @@
 var WorkID = {
-	Load: 0
+	
 }
 
 onmessage = function (Event) {
 	switch (Event.data.WorkID) {
-		case WorkID.Load:
-			var Reader = new FileReaderSync();
-				Reader.readAsText(Event.data.Data);
-				
-			postMessage({
-				WorkID: WorkID.Load,
-				Data: JSON.parse(Reader.result)
-			});
-			
-			break;
 	}
 }
