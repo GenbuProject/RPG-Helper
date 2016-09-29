b1eaeb0b28fffd7206f55f2a993b42fd00dad07c
GenbuHase <hasegenbu@gmail.com>
2016-09-29 12:23:40 +0900

    OK

--- Release/RPG Helper Release 1.4.js
+++ Release/RPG Helper Release 1.4.js
@@ -801,22 +801,6 @@ var RPGHelper = function () {
 				document.removeEventListener("keydown", R.THIS.PadFunc, false);
 			}
 			
-			if (R.THIS.Map.Timers != []) {
-				for (var i = 0; i < R.THIS.Map.Timers.length; i++) {
-					clearInterval(R.THIS.Map.Timers[i]);
-				}
-				
-				R.THIS.Map.Timers = [];
-			}
-			
-			if (R.THIS.Map.Keys != []) {
-				for (var i = 0; i < R.THIS.Map.Keys.length; i++) {
-					document.removeEventListener("keydown", R.THIS.Map.Keys[i], false);
-				}
-				
-				R.THIS.Map.Keys = [];
-			}
-			
 			R.THIS.MoveFuc = function (Key) {
 				var MemPos = [Key == Resource.SystemData.Key.Left ? CharaPos[0] - 1 : Key == Resource.SystemData.Key.Right ? CharaPos[0] + 1 : CharaPos[0], Key == Resource.SystemData.Key.Up ? CharaPos[1] - 1 : Key == Resource.SystemData.Key.Down ? CharaPos[1] + 1 : CharaPos[1]];
 				var Direction = Key == Resource.SystemData.Key.Up ? R.DIRECTION.N : Key == Resource.SystemData.Key.Down ? R.DIRECTION.S : Key == Resource.SystemData.Key.Left ? R.DIRECTION.W : Key == Resource.SystemData.Key.Right ? R.DIRECTION.E : null;
