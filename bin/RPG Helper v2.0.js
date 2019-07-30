8e9d4b42cec43e6a248058f41ed5c577dc7361f8
Genbu Hase <hasegenbu@gmail.com>
2019-07-30 17:44:21 +0900

    ひとまず音源の再生機構を実装

--- bin/RPG Helper v2.0.js
+++ bin/RPG Helper v2.0.js
@@ -97,7 +97,8 @@ const RPGHelper = (() => {
 		constructor (rpghelper) {
 			this.rpghelper = rpghelper;
 			this.ctx = new AudioContext();
-
+			
+			/** @type { { [audioType: string]: Map<string, AudioBuffer> } } */
 			this.buffers = {
 				[RPGHelper.LOADTYPE.BGM]: new Map(),
 				[RPGHelper.LOADTYPE.SE]: new Map()
@@ -105,27 +106,40 @@ const RPGHelper = (() => {
 		}
 
 		/**
+		 * 指定された音源のバッファを取得します
+		 * 
+		 * @param {string} audioType
+		 * @param {string} file
+		 * 
+		 * @return {undefined | AudioBuffer}
+		 */
+		get (audioType, file) {
+			const { rpghelper } = this;
+			rpghelper._checkInitialized();
+
+			return this.buffers[audioType].get(this._getFileUrl(audioType, file));
+		}
+
+		/**
 		 * 指定された音源を読み込みます
 		 * 
 		 * @param {string} audioType
 		 * @param {string} file
+		 * 
+		 * @return {Promise<AudioBuffer>}
 		 */
 		load (audioType, file) {
 			const { rpghelper } = this;
 			rpghelper._checkInitialized();
 
-			switch (audioType) {
-				default: throw new RPGHelperError(RPGHelper.ERRORS.AUDIO["UNACCEPTED_TYPE"]);
-				
-				case RPGHelper.LOADTYPE.BGM:
-				case RPGHelper.LOADTYPE.SE:
-					file = this.getFileUrl(audioType, file);
-					break;
+			file = this._getFileUrl(audioType, file);
+
+			if (this.buffers[audioType].has(file)) {
+				console.warn(new RPGHelperError(RPGHelper.ERRORS.LOAD["DUPLICATED-AUDIO"]));
+				return Promise.resolve(this.buffers[audioType].get(file));
 			}
 
-			if (this.buffers[audioType].has(file)) return console.warn(new RPGHelperError(RPGHelper.ERRORS.LOAD["DUPLICATED-AUDIO"]));
-
-			fetch(file).then(resp => {
+			return fetch(file).then(resp => {
 				if (resp.status !== 200) throw new RPGHelperError(RPGHelper.ERRORS.LOAD.NOT_FOUND);
 				return resp.arrayBuffer();
 			}).then(buffer => this.ctx.decodeAudioData(buffer)).catch(error => {
@@ -133,22 +147,42 @@ const RPGHelper = (() => {
 				throw new RPGHelperError(RPGHelper.ERRORS.AUDIO["FAILURE-COMPILE"]);
 			}).then(decodedBuffer => {
 				this.buffers[audioType].set(file, decodedBuffer);
+				return decodedBuffer;
 			});
-
-			console.log(file);
 		}
 
 		/**
-		 * 指定された音源のパスを絶対パスに変換します
+		 * 指定された音源を再生します
 		 * 
+		 * @param {string} audioType
+		 * @param {string} file
+		 */
+		play (audioType, file) {
+			const { rpghelper, ctx } = this;
+			rpghelper._checkInitialized();
+
+			const buffer = this.get(audioType, file);
+			if (!buffer) throw new RPGHelperError(RPGHelper.ERRORS.AUDIO.NOT_LOADED);
+
+			const source = ctx.createBufferSource();
+			source.buffer = buffer;
+			source; // ToDo: オプションと紐付けるために引数を全部変更
+
+			source.connect(ctx.destination);
+			source.start(0);
+		}
+
+		/**
 		 * @param {string} audioType
 		 * @param {string} file
 		 * 
 		 * @return {string}
 		 */
-		getFileUrl (audioType, file) {
+		_getFileUrl (audioType, file) {
 			const { rpghelper } = this;
 			rpghelper._checkInitialized();
+
+			if (!(audioType in this.buffers)) throw new RPGHelperError(RPGHelper.ERRORS.AUDIO["UNACCEPTED_TYPE"]);
 
 			const { directories } = rpghelper.data.projectField;
 
@@ -214,6 +248,7 @@ const RPGHelper = (() => {
 		},
 		
 		AUDIO: {
+			"NOT_LOADED": "音源が読み込まれていません",
 			"UNACCEPTED_TYPE": "音源の種類が無効です",
 			"FAILURE-COMPILE": "無効な音源ファイルです"
 		}
